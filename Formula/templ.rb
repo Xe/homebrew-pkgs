29d98d5e676d08caa2eb8e6f6384878caf117901
Xe Iaso <me@xeiaso.net>
2024-07-01 16:29:53 +0000

    templ: add 0.2.731 bottle.

--- Formula/templ.rb
+++ Formula/templ.rb
@@ -5,6 +5,12 @@ class Templ < Formula
   sha256 "229c84a6e48f160d7167131e3fc4a82662a421cb4142f130d1e8d593f81f3595"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/Xe/homebrew-pkgs/releases/download/templ-0.2.731"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "7817399c2265e6abfaec1c14a62c9150085f314cedba66773c0663ebcac2f1d5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "3e7efeea9f20e0828775f3e939e051fde624b94de849bb6debcac8206786ac51"
+  end
+
   depends_on "go" => :build
 
   def install
